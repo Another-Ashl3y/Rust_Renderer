e67aba2d53b1e5c398512914a3692e2beb404749
Another-Ashl3y <another.ashl3y@gmail.com>
2024-03-15 22:56:35 +0000

    Code fixed

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use shapes::{Ray, Triangle, Vec3};
 mod shapes;
 
 fn main() {
-    let face: Triangle = Triangle::new(Vec3{x:0.0,y:0.0,z:1.0},Vec3{x:1.0,y:0.0,z:1.0},Vec3{x:1.0,y:1.0,z:1.0});
-    let ray: Ray = Ray { point: Vec3{x:0.0,y:0.0,z:0.0}, vector: Vec3{x:0.5,y:1.0,z:0.5}};
+    let face: Triangle = Triangle::new(
+        Vec3{x:-10.0,y:-10.0,z:1.0},
+        Vec3{x:-10.0,y:10.0,z:1.0},
+        Vec3{x:10.0,y:10.0,z:1.0}
+    );
+    let ray: Ray = Ray { point: Vec3{x:0.0,y:0.0,z:0.0}, vector: Vec3{x:0.0,y:0.0,z:1.0}};
     println!("{}", face.intersects(ray));
 }
--- src/shapes.rs
+++ src/shapes.rs
@@ -18,7 +18,6 @@ pub struct Ray {
     pub vector: Vec3
 }
 
-
 pub struct Camera {
     pub position: Vec3,
     pub width: f32,
@@ -33,6 +32,12 @@ pub struct Triangle {
     pub C: Vec3, // Point C
     pub N: Vec3,  // Normal Vector
     pub center: Vec3
+}
+
+impl Ray {
+    pub fn get_point(&self, t:f32) -> Vec3 {
+        Vec3 {x: self.point.x + (self.vector.x*t), y: self.point.y + (self.vector.y*t), z: self.point.z + (self.vector.z*t)}
+    }
 }
 
 impl Vec3 {
@@ -58,12 +63,15 @@ impl Vec3 {
         self.z = ((self.y-origin.y)*angle.sin()+(self.z-origin.z)*angle.cos())+origin.z;
     }
     pub fn len(&self) -> f32 {
-        (self.x*self.x+self.y*self.y+self.z*self.z)*0.5 // Return the length of vector
+        (self.x.powi(2)+self.y.powi(2)+self.z.powi(2)).powf(0.5) // Return the length of vector
     }
-    pub fn normalize(&mut self) {
-        self.x = self.x/self.len();
-        self.y = self.y/self.len();
-        self.z = self.z/self.len();
+    pub fn normalize(&self) -> Vec3{
+        let length:f32 = self.len();
+        Vec3{
+            x: self.x/length,
+            y: self.y/length,
+            z: self.z/length
+        }
     }
     pub fn dot(A:&Vec3, B:&Vec3) -> f32 {
         A.x*B.x+A.y*B.y+A.z*B.z
@@ -75,6 +83,13 @@ impl Vec3 {
             z: A.z - B.z
         }
     }
+    pub fn x_to_midpoint(A: &Vec3, B: &Vec3, C:&Vec3) -> Vec3 {
+        let mid = Vec3::midpoint(B, C);
+        Vec3::sub(&mid, A)
+    }
+    pub fn midpoint(A: &Vec3, B: &Vec3) -> Vec3{
+        Vec3{x: (A.x+B.x)/2.0, y: (A.y+B.y)/2.0, z: (A.z+B.z)/2.0}
+    }
 }
 
 impl Triangle { // A, B, C, Normal, Center
@@ -82,10 +97,14 @@ impl Triangle { // A, B, C, Normal, Center
         let center = Vec3 {   x: (A.x + B.x + C.x)/3.0,
                                     y: (A.y + B.y + C.y)/3.0,
                                     z: (A.z + B.z + C.z)/3.0};
-        let mut normal: Vec3 = Vec3 {   x: A.y*B.z-A.z*B.y,
-                                    y: A.z*B.x-A.x*B.z,
-                                    z: A.x*B.y-A.y*B.x};
-        normal.normalize();
+        let a = Vec3::sub(&B, &A);
+        let b = Vec3::sub(&C, &A);
+
+        let mut normal: Vec3 = Vec3 {   
+            x: a.y*b.z-a.z*b.y,
+            y: a.z*b.x-a.x*b.z,
+            z: a.x*b.y-a.y*b.x};
+        normal = normal.normalize();
 
         Triangle {
             A: A, B: B, C: C,
@@ -93,81 +112,24 @@ impl Triangle { // A, B, C, Normal, Center
             center: center
         }
     }
-
     pub fn intersects(&self, ray: Ray) -> bool {
-        // point used for formula will be self.A
-        // Normal.x(x - A.x) + Normal.y(y - A.y) + Normal.z(z - A.z) = 0
-        // Normal.x(-A.x) + Normal.y(-A.y) + Normal.z(-A.z) + Normal.x(ray.point.x) + Normal.y(ray.point.y) + Normal.z(ray.point.z) = 
-        // t = (Nx(X-Xi)+Ny(Y-Yi)+Nz(Z-Zi))/(Nx*Ax+Ny*Ay+Nz*Az)
         let denominator = Vec3::dot(&self.N, &ray.vector);
-        println!("Normal {} {} {}", self.N.x, self.N.y, self.N.z);
         println!("Den {}", denominator);
-        if denominator == 0.0 {
-            return false;
-        }
+        println!("Normal {} {} {}", self.N.x, self.N.y, self.N.z);
+        if denominator == 0.0 {return false;}
+
         let t: f32 = Vec3::dot(&self.N, &Vec3::sub(&self.A, &ray.point))/denominator;
 
-        let intercept: Vec3 = Vec3 {
-            x: ray.point.x + (ray.vector.x*t),
-            y: ray.point.y + (ray.vector.y*t),
-            z: ray.point.z + (ray.vector.z*t)
-        };
-        let AV:Vec3 = Vec3 {
-            x: (self.B.x+self.C.x)/2.0,
-            y: (self.B.y+self.C.y)/2.0,
-            z: (self.B.z+self.C.z)/2.0};
-        let BV:Vec3 = Vec3 {
-            x: (self.A.x+self.C.x)/2.0,
-            y: (self.A.y+self.C.y)/2.0,
-            z: (self.A.z+self.C.z)/2.0};
-        let CV:Vec3 = Vec3 {
-            x: (self.A.x+self.B.x)/2.0,
-            y: (self.A.y+self.B.y)/2.0,
-            z: (self.A.z+self.B.z)/2.0};
+        let intercept: Vec3 = ray.get_point(t);
 
-        let a: f32 = Vec3::dot( // Calculate a
-            &AV, 
-            &Vec3 {
-            x: intercept.x - self.A.x,
-            y: intercept.y - self.A.y,
-            z: intercept.z - self.A.z}
-        )/Vec3::dot(
-            &AV,
-            &Vec3 {
-                x: self.B.x - self.A.x,
-                y: self.B.y - self.A.y,
-                z: self.B.z - self.A.z
-            }
-        );
-        let b: f32 = Vec3::dot( // Calculate b
-            &BV, 
-            &Vec3 {
-            x: intercept.x - self.B.x,
-            y: intercept.y - self.B.y,
-            z: intercept.z - self.B.z}
-        )/Vec3::dot(
-            &BV,
-            &Vec3 {
-                x: self.C.x - self.B.x,
-                y: self.C.y - self.B.y,
-                z: self.C.z - self.B.z
-            }
-        );
-        let c: f32 = Vec3::dot( // Calculate c
-            &CV, 
-            &Vec3 {
-            x: intercept.x - self.C.x,
-            y: intercept.y - self.C.y,
-            z: intercept.z - self.C.z})/Vec3::dot(
-            &CV,
-            &Vec3 {
-                x: self.A.x - self.C.x,
-                y: self.A.y - self.C.y,
-                z: self.A.z - self.C.z
-            }
-        );
-        println!("a {}",a);
-        if a >= 0.0 && b >= 0.0 && c >= 0.0 && a+b+c < 1.0 { // determine if it is inside the triangle using barycentric coordinates
+        let AV:Vec3 = Vec3::x_to_midpoint(&self.A, &self.B, &self.C);
+        println!("V: {} {} {}", AV.x, AV.y, AV.z);
+
+        let a =    Vec3::dot(&AV, &Vec3::sub(&intercept, &self.A))
+                        /
+                        Vec3::dot(&AV, &Vec3::sub(&self.B, &self.A));
+
+        if a >= 0.0 && a < 1.0 { // determine if it is inside the triangle using barycentric coordinates
             return true;
         }
         false
